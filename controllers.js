a146c204076acb858b18abb712f90c2319e38315
Matilda Ericsson <tilda.ericson@gmail.com>
2014-11-03 10:21:13 +0100

    hämta toaletter från controller.js och ta in i index

--- controllers.js
+++ controllers.js
@@ -1,33 +1,33 @@
-var myApp = angular.module('myApp',[]);
+ï»¿var myApp = angular.module('myApp',[]);
 
 	myApp.controller('MyController', function MyController($scope) {
 
 		$scope.toaletter = [
 			{
-				”namn”: ”City Avenyn”,
-				”adress”: ”Kungsportsavenyn 34”,
-				”oppettider”: ”07-21”,
-				”gratis”: ”Ja”,
-				”handikappanpassad”: ”Ja”,
-				”skotbord”: ”Ja"
+				'namn': 'City Avenyn',
+				'adress': 'Kungsportsavenyn 34',
+				'oppettider': '07-21',
+				'gratis': 'Ja',
+				'handikappanpassad': 'Ja',
+				'skotbord': 'Ja'
 			},
 
 			{
-				”namn”: ”Stadsmuseet”,
-				”adress”: ”Norra Hamngatan 12”,
-				”oppettider”: ”Dygnet runt”,
-				”gratis”: ”Ja”,
-				”handikappanpassad”: ”Ja”,
-				”skotbord”: ”Ja"
+				'namn': 'Stadsmuseet',
+				'adress': 'Norra Hamngatan 12',
+				'oppettider': 'Dygnet runt',
+				'gratis': 'Ja',
+				'handikappanpassad': 'Ja',
+				'skotbord': 'Ja'
 			},
 
 			{
-				”namn”: ”Hemköp”,
-				”adress”: ”Hvitfeldtsplatsen 3”,
-				”oppettider”: ”08-22”,
-				”gratis”: ”Nej”,
-				”handikappanpassad”: ”Ja”,
-				”skotbord”: ”Ja"
+				'namn': 'HemkÃ¶p',
+				'adress': 'Hvitfeldtsplatsen 3',
+				'oppettider': '08-22',
+				'gratis': 'Nej',
+				'handikappanpassad': 'Ja',
+				'skotbord': 'Ja'
 			}
 			
 

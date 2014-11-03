12385c31dc2647f7b0f35a693c5db84cfd60497c
Matilda Ericsson <tilda.ericson@gmail.com>
2014-11-03 10:58:03 +0100

    börjat hämta med  och extern json till index

--- controllers.js
+++ controllers.js
@@ -1,35 +1,8 @@
 ﻿var myApp = angular.module('myApp',[]);
 
-	myApp.controller('MyController', function MyController($scope) {
+	myApp.controller('MyController', function MyController($scope, $http) {
+		$http.get('toaletter.json').success(function(data) {
+			$scope.toaletter = data;
+		});
 
-		$scope.toaletter = [
-			{
-				'namn': 'City Avenyn',
-				'adress': 'Kungsportsavenyn 34',
-				'oppettider': '07-21',
-				'gratis': 'Ja',
-				'handikappanpassad': 'Ja',
-				'skotbord': 'Ja'
-			},
-
-			{
-				'namn': 'Stadsmuseet',
-				'adress': 'Norra Hamngatan 12',
-				'oppettider': 'Dygnet runt',
-				'gratis': 'Ja',
-				'handikappanpassad': 'Ja',
-				'skotbord': 'Ja'
-			},
-
-			{
-				'namn': 'Hemköp',
-				'adress': 'Hvitfeldtsplatsen 3',
-				'oppettider': '08-22',
-				'gratis': 'Nej',
-				'handikappanpassad': 'Ja',
-				'skotbord': 'Ja'
-			}
-			
-
-		]
 });

9177ea1d74d40c5b18d2e2c968626a7bb4c197e8
Matilda Ericsson <tilda.ericson@gmail.com>
2014-11-05 22:54:24 +0100

    trixat, fixat, haxat och baxat. Med andra ord fixat med markers för de olika sidorna

--- js/controllers.js
+++ js/controllers.js
@@ -21,9 +21,11 @@ var mapOptions = {
     $scope.markers = [];
     
     var infoWindow = new google.maps.InfoWindow();
-    
+	
+	var currentPage = document.URL.slice(-1); //Hämtar vilken sidan man befinner sig på genom att ta sista tecknet på URL-texten. OBS FULLÖSNING!
+	
     //Funktionen createMarker anropas nedan när vi läser in vår datafil.
-    var createMarker = function (info){
+    var createMarker = function (info, index){
         console.log('createMarker');
         console.log(info);
         var marker = new google.maps.Marker({
@@ -37,8 +39,11 @@ var mapOptions = {
         google.maps.event.addListener(marker, 'click', function(){
             infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
             infoWindow.open($scope.map, marker);
+			if(index>=0 && index!=currentPage){ //om markern inte är currentPosition-markern och om markern inte är för nuvarande sidan
+				window.location = "index.html#/undersida/"+index; //När man klickar på en marker kommer man till dess undersida
+				location.reload();
+			}
         });
-        
         $scope.markers.push(marker);    
     }  
 
@@ -54,9 +59,13 @@ var mapOptions = {
     $http.get('js/toaletter.json').success(function(data){
         $log.debug(data);
         $scope.retur_data = data;
-        for (i = 0; i < data.length; i++){
-            createMarker(data[i]);
-        }
+		if (currentPage == "t"){ // kollar om man är på huvudsidan
+			for (i = 0; i < data.length; i++){
+				createMarker(data[i], i);
+			}
+		}else{ // om man inte är på huvudsidan skapas bara markers för den utvalda sidan
+			createMarker(data[currentPage], currentPage);
+		}
     }); 
     
     //Hämtar nuvarande postition och lägger in info i debug-div. Samt centrerar om kartan.
@@ -76,7 +85,7 @@ var mapOptions = {
 			ort: ""
 		};
 		
-		createMarker(currentPosition);
+		createMarker(currentPosition, -1);
     }
 
     //Hanterar felmeddelanden.
@@ -125,103 +134,5 @@ toaControllers.controller('UndersidaController', function ($scope, $http, $route
         });
     
 
-//Kod för att endast hämta en marker och visa den för den valda toan på undersidan	
-toaControllers.controller('MarkerController', function MarkerController ($scope, $http, $log){
-
-	var mapOptions = {
-        zoom: 13,
-        center: new google.maps.LatLng(57.709292 , 11.972647),  //Centrerar på Göteborg, nånstans i centrum. Geo location längre ner.
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    
-    $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		
-		$scope.markers = [];
-    
-    var infoWindow = new google.maps.InfoWindow();
-    
-    //Funktionen createMarker anropas nedan när vi läser in vår datafil.
-    var createMarker = function (info){
-        console.log('createMarker');
-        console.log(info);
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            position: new google.maps.LatLng(info.lat, info.long), //om, man kan få dom här att hämta lat och long genom index för vald toa
-            title: info.namn,
-            platsen: info.ort
-        });
-        marker.content = '<div class="infoWindowContent">' + info.ort + '</div>';
-        
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
-        
-        $scope.markers.push(marker);    
-    }  
-
-    //Vid klick av länk så ska infoFönstret visas
-    $scope.openInfoWindow = function(e, selectedMarker){
-        console.log('openInfoWindow');
-        console.log(selectedMarker);
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
-    }
-    
-    //Hämtar datafilen och lägger ut markers utifrån den informations som kommer tillbaka
-    $http.get('js/toaletter.json').success(function(data){
-        $log.debug(data);
-        $scope.retur_data = data;
-        for (i = 0; i < data.length; i++){
-            createMarker(data[i]);
-        }
-    }); 
-    
-    //Hämtar nuvarande postition och lägger in info i debug-div. Samt centrerar om kartan.
-    $scope.showPosition = function (position) {
-        $scope.lat = position.coords.latitude;
-        $scope.lng = position.coords.longitude;
-        $scope.accuracy = position.coords.accuracy;
-        $scope.$apply();
-
-        var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
-        $scope.map.setCenter(latlng);
-    }
-
-    //Hanterar felmeddelanden.
-    $scope.showError = function (error) {
-        switch (error.code) {
-            case error.PERMISSION_DENIED:
-                $scope.error = "User denied the request for Geolocation."
-                break;
-            case error.POSITION_UNAVAILABLE:
-                $scope.error = "Location information is unavailable."
-                break;
-            case error.TIMEOUT:
-                $scope.error = "The request to get user location timed out."
-                break;
-            case error.UNKNOWN_ERROR:
-                $scope.error = "An unknown error occurred."
-                break;
-        }
-        $scope.$apply();
-    }
-    
-    //Hämtar nuvarande platss
-    $scope.getLocation = function () {
-        if (navigator.geolocation) {
-            //Anropar funtionen showPosition ovan om GeoLocation stöds av webbläsaren
-            navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
-        }
-        else {
-            $scope.error = "Geolocation is not supported by this browser.";
-        }
-    }
-
-    //Kör ovanstående funktion getLocation.
-    $scope.getLocation();
-    
 });
 
-
-});

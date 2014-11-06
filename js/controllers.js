c60e70bfeaf43744aed960a3fcc91a0d6dff9741
Matilda Ericsson <tilda.ericson@gmail.com>
2014-11-06 12:32:47 +0100

    fixar med vägbeskrivning

--- js/controllers.js
+++ js/controllers.js
@@ -9,7 +9,7 @@ var toaControllers = angular.module('toaControllers', []);
 	});
     
 toaControllers.controller('AppController', function AppController ($scope, $http, $log){
-    
+
 var mapOptions = {
         zoom: 13,
         center: new google.maps.LatLng(57.709292 , 11.972647),  //Centrerar på Göteborg, nånstans i centrum. Geo location längre ner.
@@ -23,6 +23,28 @@ var mapOptions = {
     var infoWindow = new google.maps.InfoWindow();
 	
 	var currentPage = document.URL.slice(-1); //Hämtar vilken sidan man befinner sig på genom att ta sista tecknet på URL-texten. OBS FULLÖSNING!
+
+	var latlng;
+
+
+	function computeTotalDistance(result) {
+		var total = 0, i,
+			myroute = result.routes[0];
+		for (i = 0; i < myroute.legs.length; i++) {
+			total += myroute.legs[i].distance.value;
+		}
+		total = total / 1000;
+		$scope.$apply(function () {
+			$scope.totalKm = total;
+		});
+	}
+
+	
+	
+	
+	
+    //kod som var innan directions kom in
+
 	
     //Funktionen createMarker anropas nedan när vi läser in vår datafil.
     var createMarker = function (info, index){
@@ -75,7 +97,7 @@ var mapOptions = {
         $scope.accuracy = position.coords.accuracy;
         $scope.$apply();
 
-        var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
+        latlng = new google.maps.LatLng($scope.lat, $scope.lng);
         $scope.map.setCenter(latlng);
 		
 		var currentPosition = { //Variabel för geolocation marker på kartan
@@ -112,14 +134,109 @@ var mapOptions = {
         if (navigator.geolocation) {
             //Anropar funtionen showPosition ovan om GeoLocation stöds av webbläsaren
             navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+			
+			return navigator.geolocation.getCurrentPosition(function (position) {
+			var pos = new google.maps.LatLng(position.coords.latitude,
+											 position.coords.longitude);
+			console.log(pos);
+			return pos; });
         }
         else {
             $scope.error = "Geolocation is not supported by this browser.";
         }
+		
     }
 
     //Kör ovanstående funktion getLocation.
     $scope.getLocation();
+	
+	//Directions
+	if (currentPage != "t"){ // om den nuvarande sidan inte är t så körs directionsgrejen
+		// defaults for your business location and blurb
+		var streetAddress = "5111 47 St NE  Calgary, AB T3J 3R2",
+			Location = new google.maps.LatLng(51.098945, -113.970889),
+			businessWriteup = "<b>Calgary Police Service</b><br/>Calgary's Finest<br/>",
+			defaultFromAddress = 'Calgary',
+			businessTitle = "Calgary Police Service",
+			directionsService = new google.maps.DirectionsService(),
+			directionsDisplay = new google.maps.DirectionsRenderer({
+				draggable: true
+			});
+
+		// add your fixed business marker
+		var contentString = businessWriteup + streetAddress
+		  marker = new google.maps.Marker({
+			  position: Location,
+			  map: $scope.map,
+			  title: businessTitle,
+			  animation: google.maps.Animation.DROP
+		  });
+		// show info Window
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+		google.maps.event.addListener(marker, 'click', function () {
+			infowindow.open($scope.map, marker);
+		});
+
+		directionsDisplay.setMap($scope.map);
+		directionsDisplay.setPanel(document.getElementById('directions'));
+
+		console.log($scope.getLocation());
+		$scope.fromAddress = $scope.getLocation (); 
+		$scope.selectedOption = 'Driving';
+		$scope.options = ['Driving', 'Walking', 'Bicycling', 'Transit'];
+		$scope.totalKm = 0;
+
+		$scope.setDirections = function () {
+			var selectedMode = $scope.selectedOption.toUpperCase() || 'DRIVING',
+				from = $scope.fromAddress || defaultFromAddress,
+				request = {
+					origin: from,
+					destination: streetAddress,
+					travelMode: selectedMode,
+					provideRouteAlternatives: true,
+					unitSystem: google.maps.UnitSystem.METRIC,
+					optimizeWaypoints: true
+				};
+			if (selectedMode === 'TRANSIT') {
+				request.transitOptions = {
+					departureTime: new Date()
+				};
+			}
+
+			directionsService.route(request, function (response, status) {
+				if (status === google.maps.DirectionsStatus.OK) {
+					directionsDisplay.setDirections(response);
+				} else {
+					toastr.error(status);
+				}
+			});
+		}
+
+
+		google.maps.event.addListener(directionsDisplay, 'directions_changed', function () {
+
+			computeTotalDistance(directionsDisplay.directions);
+			try {
+				if (directionsDisplay.directions.routes[0].legs[0]) {
+
+					$scope.$apply(function () {
+						$scope.fromAddress = directionsDisplay.directions.routes[0].legs[0].start_address;
+					});
+				}
+			} catch (e) { }
+		});
+
+		// fire it up initially
+
+			$scope.setDirections();
+			
+			
+		// watch if the mode has changed
+		$scope.$watch('selectedOption', function (newValue, oldValue) { $scope.setDirections(); });
+	
+	} //slut på ifsatsen runt directions
     
 });
 
